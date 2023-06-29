c768c4bac0b6de9d233086feb737f4200e2c550c
Tyler Sehr <tyler@cookbook.dev>
2023-06-28 17:07:21 -0700

    next test

--- contracts/simple-token.sol
+++ contracts/simple-token.sol
@@ -1,23 +1,8 @@
-/*
-
- ██████  ██████   ██████  ██   ██ ██████   ██████   ██████  ██   ██    ██████  ███████ ██    ██
-██      ██    ██ ██    ██ ██  ██  ██   ██ ██    ██ ██    ██ ██  ██     ██   ██ ██      ██    ██
-██      ██    ██ ██    ██ █████   ██████  ██    ██ ██    ██ █████      ██   ██ █████   ██    ██
-██      ██    ██ ██    ██ ██  ██  ██   ██ ██    ██ ██    ██ ██  ██     ██   ██ ██       ██  ██
- ██████  ██████   ██████  ██   ██ ██████   ██████   ██████  ██   ██ ██ ██████  ███████   ████
-
-Find any smart contract, and build your project faster: https://www.cookbook.dev
-Twitter: https://twitter.com/cookbook_dev
-Discord: https://discord.gg/WzsfPcfHrk
-
-Find this contract on Cookbook: https://www.cookbook.dev/contracts/simple-token?utm=code
-*/
-
 // SPDX-License-Identifier: UNLICENSED
 
 pragma solidity ^0.8.10;
 
-import "./dependencies/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 /**
  * @title Simple Token
